db92a991256435e7d92a72e174ae9847926cc621
tahdi-islam <tshdi60@gmail.com>
2022-11-06 01:12:01 +0600

    customize the spinner

--- src/Router/RequireAuth.jsx
+++ src/Router/RequireAuth.jsx
@@ -7,8 +7,8 @@ const RequireAuth = ({children}) => {
     const location = useLocation()
     if(loading)
     {
-        return <div>
-            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
+        return <div className='flex justify-center py-20'>
+            <div className="w-10 h-10 border-4 border-dotted rounded-full animate-spin border-blue-500"></div>
         </div>
     }
     if(user && user.uid){
