5f37afc3bbf1aface341d98bbc64d0c7df90464c
tahdi-islam <tshdi60@gmail.com>
2022-11-06 21:33:09 +0600

    show all post in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SinglePost from "./SinglePost";
 
 const Home = () => {
-    return (
-        <div>
-          This is Home  
-        </div>
-    );
+  const [posts, setPosts] = useState([])
+
+  // load all post
+  useEffect(() => {
+    axios.get("http://localhost:5000/posts")
+      .then(res => setPosts(res.data))
+      .catch(err => console.log(err))
+  }, [])
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+      {
+        posts.map(post => <SinglePost key={post._id} post={post}/>)
+      }
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Pages/Profile/Post.jsx
+++ src/Pages/Profile/Post.jsx
@@ -11,20 +11,20 @@ const Post = ({ post }) => {
     const {imgLink, _id, postText} = post;
     return (
         <div>
-            <div className="w-full p-8 grid grid-cols-3 bg-gray-200 text-gray-800">
-                <div color="blue" className="h-56 col-span-1">
+            <div className="w-full p-8 grid grid-cols-1 md:grid-cols-3 bg-gray-200 text-gray-800">
+                <div color="blue" className="h-56 col-span-1 md:col-span-1">
                     <img
                         src={imgLink}
                         alt="img-blur-shadow"
-                        className="h-full w-full"
+                        className="h-60 w-60 rounded-md"
                     />
                 </div>
-                <div className="text-justify col-span-2">
-                    <h3 className="mb-2 text-3xl font-bold">
+                <div className="col-span-1 md:col-span-2">
+                    <h3 className="mb-2 text-center text-3xl font-bold">
                         Cozy 5 Stars Apartment
                     </h3>
-                    <p>
-                        {postText}
+                    <p className='text-justify px-4'>
+                        {postText.length >= 430 ? <>{postText.slice(0,430)}</> : postText}
                     </p>
                 </div>
             </div>
